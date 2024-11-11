2f544cd81bce8aa00ac3d12452d2489f812ca428
xcode75 <frainzy1477@protonmail.com>
2024-11-11 20:34:23 +0000

    Update XMPlus.sh

--- XMPlus.sh
+++ XMPlus.sh
@@ -111,152 +111,20 @@ install() {
 }
 
 update() {
-    systemctl stop XMPlus
-    if [[ -e /usr/local/XMPlus/ ]]; then
-        rm /usr/local/XMPlus/ -rf
-    fi
-	
-	if [[ -f /usr/bin/XMPlus ]]; then
-		rm /usr/bin/XMPlus -f
-	fi
-	
-	if [[ -f /usr/bin/xmplus ]]; then
-		rm /usr/bin/xmplus -f
-	fi
-	
-    mkdir /usr/local/XMPlus/ -p
-	cd /usr/local/XMPlus/
-
-    if  [ $# == 0 ] ;then
-        last_version=$(curl -Ls "https://api.github.com/repos/XMPlusDev/XMPlus/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}Failed to detect the XMPlus version, it may be because of Github API limit, please try again later, or manually specify the XMPlus version to install${plain}"
-            exit 1
-        fi
-        echo -e "XMPlus latest version detected：${last_version}，Start Installation"
-        wget -N --no-check-certificate -O /usr/local/XMPlus/XMPlus-linux.zip https://github.com/XMPlusDev/XMPlus/releases/download/${last_version}/XMPlus-linux-${kernelArch}.zip
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}Downloading XMPlus failed，Please make sure your server can download github file${plain}"
-            exit 1
-        fi
+    if [[ $# == 0 ]]; then
+        echo && echo -n -e "Enter the specified version (default latest version): " && read version
     else
-        last_version=$1
-        url="https://github.com/XMPlusDev/XMPlus/releases/download/${last_version}/XMPlus-linux-${kernelArch}.zip"
-        echo -e "Start Installation XMPlus v$1"
-        wget -N --no-check-certificate -O /usr/local/XMPlus/XMPlus-linux.zip ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}Downloading XMPlus v$1 failed, make sure this version exists${plain}"
-            exit 1
-        fi
+        version=$2
+    fi
+    bash <(curl -Ls https://raw.githubusercontent.com/XMPlusDev/XMPlus/scripts/install.sh) $version
+    if [[ $? == 0 ]]; then
+        echo -e "${green}The update is complete and XMPlus has restarted automatically, please use XMPlus log to view the operation log${plain}"
+        exit
     fi
 
-    unzip XMPlus-linux.zip
-    rm XMPlus-linux.zip -f
-    chmod +x XMPlus
-	
-    if [ -e "/etc/systemd/system/" ] ; then
-		if [ -f "/etc/systemd/system/XMPlus.service" ] ; then
-			systemctl stop XMPlus
-			systemctl disable XMPlus
-		    rm /etc/systemd/system/XMPlus.service -f
-		fi
-		
-		file="https://raw.githubusercontent.com/XMPlusDev/XMPlus/scripts/XMPlus.service"
-		wget -N --no-check-certificate -O /etc/systemd/system/XMPlus.service ${file}
-		systemctl daemon-reload
-		systemctl stop XMPlus
-		systemctl enable XMPlus
-    elif [ -e "/usr/sbin/rc-service" ] ; then
-		if [ -f "/etc/init.d/xmplus/xmplus.rc" ] ; then
-			systemctl stop XMPlus
-			systemctl disable XMPlus
-			rm /etc/init.d/xmplus/xmplus.rc -f
-		else	
-			 mkdir /etc/init.d/xmplus/ -p
-		fi
-		file="https://raw.githubusercontent.com/XMPlusDev/XMPlus/scripts/xmplus.rc"
-		wget -N --no-check-certificate -O /etc/init.d/xmplus/xmplus.rc ${file}
-		systemctl daemon-reload
-		rc-update add xmplus default 
-		rc-update --update
-		chmod +x /etc/init.d/xmplus/xmplus.rc
-		ln -s /etc/XMPlus /usr/local/etc/
-    else
-       echo "not found."
+    if [[ $# == 0 ]]; then
+        before_show_menu
     fi
-	
-    mkdir /etc/XMPlus/ -p
-	
-    echo -e "${green}XMPlus ${last_version}${plain} The installation is complete，XMPlus has restarted"
-    cp geoip.dat /etc/XMPlus/
-    cp geosite.dat /etc/XMPlus/ 
-	
-    if [[ ! -f /etc/XMPlus/dns.json ]]; then
-		cp dns.json /etc/XMPlus/
-	fi
-	if [[ ! -f /etc/XMPlus/route.json ]]; then 
-		cp route.json /etc/XMPlus/
-	fi
-	
-	if [[ ! -f /etc/XMPlus/outbound.json ]]; then
-		cp outbound.json /etc/XMPlus/
-	fi
-	
-	if [[ ! -f /etc/XMPlus/inbound.json ]]; then
-		cp inbound.json /etc/XMPlus/
-	fi
-
-	if [[ ! -f /etc/XMPlus/rulelist ]]; then
-		cp rulelist /etc/XMPlus/
-	fi
-	
-    if [[ ! -f /etc/XMPlus/config.yml ]]; then
-        cp config.yml /etc/XMPlus/
-    else
-        if [ -e "/etc/systemd/" ] ; then
-			systemctl start XMPlus
-		else
-			rc-service xmplus start
-		fi
-        sleep 2
-        check_status
-        echo -e ""
-        if [[ $? == 0 ]]; then
-            echo -e "${green}XMPlus restart successfully${plain}"
-        else
-            echo -e "${red} XMPlus May fail to start, please use [ XMPlus log ] View log information ${plain}"
-        fi
-    fi
-    
-    curl -o /usr/bin/XMPlus -Ls https://raw.githubusercontent.com/XMPlusDev/XMPlus/scripts/XMPlus.sh
-    chmod +x /usr/bin/XMPlus
-    ln -s /usr/bin/XMPlus /usr/bin/xmplus 
-    chmod +x /usr/bin/xmplus
-
-    echo -e ""
-    echo "XMPlus Management usage method: "
-    echo "------------------------------------------"
-    echo "XMPlus                    - Show menu (more features)"
-    echo "XMPlus start              - Start XMPlus"
-    echo "XMPlus stop               - Stop XMPlus"
-    echo "XMPlus restart            - Restart XMPlus"
-    echo "XMPlus status             - View XMPlus status"
-    echo "XMPlus enable             - Enable XMPlus auto-start"
-    echo "XMPlus disable            - Disable XMPlus auto-start"
-    echo "XMPlus log                - View XMPlus logs"
-    echo "XMPlus update             - Update XMPlus"
-    echo "XMPlus update vx.x.x      - Update XMPlus Specific version"
-    echo "XMPlus config             - Show configuration file content"
-    echo "XMPlus install            - Install XMPlus"
-    echo "XMPlus uninstall          - Uninstall XMPlus"
-    echo "XMPlus version            - View XMPlus version"
-    echo "XMPlus update_script      - Upgrade script"
-    echo "XMPlus warp               - Generate cloudflare warp account"
-    echo "XMPlus x25519             - enerate reality key pairs"
-    echo "------------------------------------------"
-	
-    chmod +x /usr/bin/XMPlus
-    echo -e "${green} Upgrade was successful ${plain}" && exit 0
 }
 
 config() {
